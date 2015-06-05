4469a29baf1ea298fa5566c1a3dc48b82f072584
niksanaznc <progressiven6@gmail.com>
2015-06-05 23:19:01 +0300

    Update geometry.cpp

--- geometry/geometry.cpp
+++ geometry/geometry.cpp
@@ -27,34 +27,7 @@ void testLines()
 
 int main()
 {
-	/*Point3D A(0 , 3 , 2) , B(-2 , 4 , -2) , C(1 ,2 , -1) , D(3 ,3 ,3);
-	Vector v1(1 , 1 , 1) , v2(3 , 3 , 3);
-	Plain beta(0 , 0 , -1 , 4);
-	Line a(A , B) , b(C , D);
-	Plain alpha(2 , 3 , 5 , 4);
-	Point3D X(a , beta);
-	v1.Xmultiply(v2).print();
-	v1 += v2;
-	Triangle tr(A , B , C);
-	Line aOrtho = a.getOrthoProjection(alpha);
-	alpha.commonPrint();
-	aOrtho.print();
-	LinePlain(aOrtho , alpha);*/
-
-	/*Vector v(2 , 3 , 2);
-	Point3D A(1 , 3 , 2);
-	Line l(A , v);
-	l.print();
-	l.getPlains();
-	Line a( Plain(-30 , 6 , 21 , -9) , Plain(3 , -6 , 6 , 9));
-	a.print();*/
-	
-	//testLines();
-
-	Line a(Point3D(0 , -3 , 2) , Vector(13 , 5 , -13)) , b(Point3D(-2 , 3 , 4) , Vector(1 , 1 , -1));
-	a.print(); cout<<endl;
-	b.print(); cout<<endl;
-	cout<<a.hasCommonPoint(b)<<endl;
+	testLines();
 	system("pause");
 	return 0;
 }
